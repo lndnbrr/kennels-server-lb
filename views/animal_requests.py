d423b000401456544a857de13dfda3c14033ba08
Landon Borrego <lndnborrrego@gmail.com>
2025-04-19 21:15:10 -0400

    added changes

--- views/animal_requests.py
+++ views/animal_requests.py
@@ -26,3 +26,19 @@ ANIMALS = [
 def get_all_animals():
     """Function returing ANIMALS list of dictionaries"""
     return ANIMALS
+
+# Function with a single parameter
+def get_single_animal(id):
+    """Function returing a single animal from the ANIMALS list of dictionaries"""
+    # Variable to hold the found animal, if it exists
+    requested_animal = None
+
+    # Iterate the ANIMALS list above. Very similar to the
+    # for..of loops you used in JavaScript.
+    for animal in ANIMALS:
+        # Dictionaries in Python use [] notation to find a key
+        # instead of the dot notation that JavaScript used.
+        if animal["id"] == id:
+            requested_animal = animal
+
+    return requested_animal
